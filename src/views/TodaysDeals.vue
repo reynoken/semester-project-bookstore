c770f19543b9949837e8306ffc656c102d988490
dariakalemba <103864943+dariakalemba@users.noreply.github.com>
2022-04-16 20:47:02 -0400

    Add files via upload

--- src/views/TodaysDeals.vue
+++ src/views/TodaysDeals.vue
@@ -2,32 +2,141 @@
     <div class="deals">
         <h1 id="pageTitle">The Book Store <button @click="outtahere">Logout</button> <button @click="goToCart">Cart</button> <button @click="toHome">Continue Browsing</button></h1>
         <h2>Today's Deals</h2>
+        <!-- add more elements to match wireframe -->
+        <table v-if="bookArr.length > 0 ">
+            <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+            </tr>
+            <tr v-for="(u,pos) in bookArr.slice(6,9)" :key="pos">
+                
+                <td><img :src="u.image"></td>
+                <td>{{u.title}}</td>
+                <td>Original {{u.price}}</td><td id="price"> Now $20 <button @click="saveBookToCart">Add to Cart</button></td>
+                
+            </tr>
+           
+        </table>
+        
     </div>
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue} from "vue-property-decorator";
+
+import {Component, Prop, Vue } from "vue-property-decorator";
+import {
+  getAuth,
+  onAuthStateChanged,
+  UserCredential,
+  User,
+  Auth,
+  signOut,
+  deleteUser,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import WorldTime from "@/components/world-time.vue";
+import myDB from "@/App.vue";
+import { DocumentReference, setDoc, doc, getFirestore } from "@firebase/firestore";
+import {Firestore} from "firebase/firestore";
+import { BookStoreResponse, ITBooks } from "@/datatypes";
+import axios, {AxiosResponse} from "axios";
+
+// const bookAPIUrl = "https://api.itbook.store/1.0/new";
+// const todayDealBookUrl = "https://api.itbook.store/1.0/search/mongodb";
 
 
 @Component
-export default class CheckOut extends Vue {
-
-    toHome(): void {
+export default class HomePage extends Vue {
+  bookArr: Array<ITBooks> = [];
+  userPhotoURL = "";
+  message = "";
+  auth: Auth | null = null;
+  myDB: Firestore | null = null;
+  userInfo = "";
+  mounted(): void {
+    this.auth = getAuth();
+    onAuthStateChanged(this.auth, (user: User | null) => {
+      console.log("Auth changed", user);
+      if (user) {
+        this.userPhotoURL = user.photoURL ?? "";
+        this.userInfo = `${user.displayName ?? "No Name"}`;
+      }
+    });
+    this.myDB = getFirestore(); 
+    this.loadBookData();
+  }
+  toHome(): void {
         this.$router.back();
     }
+  
+  showMessage(txt: string) {
+    this.message = txt;
+
+    // The message will automatically disappear after 5 seconds
+    setTimeout(() => {
+      this.message = "";
+    }, 5000);
+  }
+
+  outtahere(): void {
+    if (this.auth) signOut(this.auth);
+
+    // Back to the previous page
+    this.$router.back();
+  }
+
+  loadBookData(): void {
+      axios.request({
+          method: "GET",
+          url: "https://api.itbook.store/1.0/new",
+      })
+      .then((r:AxiosResponse) => r.data)
+      .then((r:BookStoreResponse) => {
+          this.bookArr.push(...r.books);
+      });
+   
+  }
+  //this function should save book to "cart" when user hits button, problem is it saves all books
+  saveBookToCart(): void {
+    const auth = getAuth();
+    const myDB = getFirestore();
+    const uid = auth.currentUser?.uid;
+    if (uid!== undefined) {
+      const dx = doc(myDB, "bookstoreuser", uid);
+      //let l: string[] = this.bookArr.map((z:ITBooks) => z.image)
+      let n: string[] = this.bookArr.map((z:ITBooks) => z.title)
+      let m: string[] = this.bookArr.map((z:ITBooks) => z.price)
+       setDoc(dx, {title: n, price: m});
+    }
+  }
+
+  goToCart(): void {
+    this.$router.push({name: "cart"});
+  }
+ 
 }
 </script>
 
+
 <style scoped>
 h2 {
-    text-align: center;
+  text-align: center;
 }
- #pageTitle {
+    #pageTitle {
         margin: auto;
-        width: 55%;
+        width: 50%;
         border: 3px solid gray;
         padding: 10px;
         margin-bottom: 25px;
         text-align: center;
+    }
+    table {
+      margin: auto;
+      text-align: center;
+    }
+
+    #price  {
+      color: brown;
     }
 </style>
