0da3029bdb6113e3913c53096c634a7d9e96a9a3
dariakalemba <103864943+dariakalemba@users.noreply.github.com>
2022-04-24 23:34:46 -0400

    Update ChartDoughnut.vue

--- src/components/ChartDoughnut.vue
+++ src/components/ChartDoughnut.vue
@@ -12,7 +12,7 @@ import { ref, computed } from "vue"
 import { DoughnutChart } from "vue-chart-3"
 import { Chart, DoughnutController, ArcElement } from "chart.js"
 Chart.register(DoughnutController, ArcElement)
-const dataValues = ref([6, 14])
+const dataValues = ref([30, 70])
 const data = computed(() => ({
   labels: ["Free", "Not free"],
   datasets: [
